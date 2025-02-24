3cfa2fcc4aee10f19aafeed4a579c1b80675a4b6
Markiza666 <3ggjs24erikma@folkuniversitetet.nu>
2025-02-24 02:07:42 +0100

    la till font på index sidan, uppdaterade vite, skrev kod i router, error, app, header och scss filer

---
+++ src/pages/Error.jsx
@@ -0,0 +1,7 @@
+function Error() {
+    return (
+        <h1>This is not the page you are loking for.</h1>
+    )
+}
+
+export default Error;
---
+++ src/router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from 'react-router-dom'; // "createBrowserRouter" används för DOM History API som uppdaterar URL
+import App from './App.jsx'
+import Error from './pages/Error.jsx';
+// import About from './pages/About/About.jsx';
+// import OrderStatus from './pages/OrderStatus/OrderStatus.jsx'
+// import Menu from './pages/Menu/Menu.jsx';
+
+const router = createBrowserRouter([
+    {
+        index: true,
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+    },
+    // {
+    //     path: 'menu',
+    //     element: <Menu />
+    // },
+    // {
+    //     path: 'about',
+    //     element: <About />
+    // },
+    // {
+    //     path: 'order-status',
+    //     element: <OrderStatus />
+    // }
+]);
+
+console.log("Router configuration:", router.routes); // Kontrollerar router-konfigurationen
+
+export default router;
